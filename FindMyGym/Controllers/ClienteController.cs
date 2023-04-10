82390626192821fd8d483380a569ebe96897d95e
SNMIRANDA@PUCE.EDU.EC <SNMIRANDA@PUCE.EDU.EC>
2023-04-09 21:01:30 -0500

    Se crearon plantillas para Controllers y Views Cliente

---
+++ FindMyGym/Controllers/ClienteController.cs
@@ -0,0 +1,54 @@
+﻿using FindMyGym.Models.ViewModels;
+using FindMyGym.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FindMyGym.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+          
+        }
+        public ActionResult Nuevo()
+        {
+
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Nuevo(TablaGimnasio model)
+        {
+
+            return View();
+        }
+
+        public ActionResult Editar(int Id)
+        {
+            
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Editar(TablaGimnasio model)
+        {
+            return Redirect("~/Gimnasio/"); ;
+        }
+        [HttpGet]
+        public ActionResult Eliminar(int Id)
+        {
+
+           
+            return Redirect("~/Gimnasio/"); 
+        }
+
+
+    }
+}
